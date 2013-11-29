6e500a4cea2b6ca9331ae2ade80944948c2e13bf
NicholasMerrill <nicholas.e.merrill@gmail.com>
2013-11-29 09:44:11 -0500

    fix

--- clean-gmail.rb
+++ clean-gmail.rb
@@ -11,12 +11,10 @@ config = IniFile.load('config.ini')
 cleanStart = Date.parse(config['times']['archive'])
 
 Gmail.connect(config['auth']['username'], config['auth']['password']) do |gmail|
-    # puts gmail.inbox.count
-    # puts gmail.inbox.count(:unread)
-
     labelName = "Archived 2013-11-29"
 
-    gmail.inbox.find(:before => cleanStart.each) do |email|
+    gmail.inbox.count(:before => cleanStart)
+    gmail.inbox.find(:before => cleanStart).each do |email|
         puts "archiving email from " + email.date
         email.label!(labelName)
         email.archive!
